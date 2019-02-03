d192b6da106fb20804a7d480d9ea44210ad83dfe
emmanouil <emmanouil@users.noreply.github.com>
2019-02-03 19:45:46 +0100

    Update question_analysis_script.py

--- single-file_snippets/question_analysis_script.py
+++ single-file_snippets/question_analysis_script.py
@@ -2,6 +2,7 @@ import os
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import json
 
 
 
@@ -11,9 +12,17 @@ print('working from' + os.getcwd())
 LIST_OF_STATEMENTS = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6']
 LIST_OF_NEGATIVE_STATEMENTS = ['S1', 'S2', 'S4', 'S6']
 LIST_OF_VIDEO_PAIRS =[['010101', '010201'],['020101', '020201'],['020102','020202'],['030101','030201'],['030102','030202']]
+ORDER_OF_VIDEOS ={'010101':0, '010201':1,'020101':2, '020201':3,'020102':4,'020202':5,'030101':6,'030201':7,'030102':8,'030202':9}
 DATA_FILE_NAME = 'res-.csv'
 EXCLUDE_IDS = [1, 2, 3]
 LIST_OF_BAR_COLOURS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b', 'g', 'r', 'c']
+LIST_OF_BAR_HATCHES = ['/', '-', '+', 'x', '\\', '*', 'o', 'O', '.']
+LIST_OF_MARKER_STYLES = ['o', '^', 'v', 's', 'D', '*']
+SHOW_PLOT1 = False
+SHOW_PLOT2 = True
+SHOW_PLOT3 = False
+
+FLUSH_MEANS_TO_FILE = False
 
 # classes
 
@@ -66,6 +75,7 @@ class Mean:
 
     def __init__(self, vid_id, statem, mean, std):
         self.id = vid_id
+        self.order = ORDER_OF_VIDEOS[vid_id]
         self.statem = statem
         self.mean = mean
         self.std = std
@@ -77,6 +87,119 @@ def isIDExluded(id_in):
         if i == int(row["'ID'"]):
             return True
     return False
+
+
+#plots
+def plot1():
+    #
+    # plot
+    # barchart
+    #  - original values ; with error bars
+    ind1 = np.arange(len(means))  # the x locations for the groups
+    width = 0.1  # the width of the bars
+    fig1, ax1 = plt.subplots()
+
+    i = 0
+    for statement in LIST_OF_STATEMENTS:
+        tmp = []
+        labels = []
+        stds = []
+        for m in means:
+            if m.statem == statement:
+                tmp.append(m.mean)
+                labels.append(m.id)
+                stds.append(m.std)
+        ind1 = np.arange(len(tmp))
+        ax1.bar(ind1 - 0.3 + ((width) * i), tmp, width, yerr=stds, color=LIST_OF_BAR_COLOURS[i % 10], label=statement)
+        i += 1
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax1.set_ylim(1, 5)   #y axis limits
+    ax1.set_ylabel('Scores')
+    ax1.set_xlabel('Video ID')
+    ax1.set_title('Scores per Statement by Video ID')
+    ax1.set_xticks(ind1)
+    ax1.set_xticklabels(labels)
+    ax1.legend()
+    ax1.grid(which = 'major', axis='y', linestyle='--')
+    plt.show()
+
+
+
+
+def plot2():
+    #
+    # plot
+    # barchart
+    # - complementary values; without error bars
+    ind2 = np.arange(len(means_compl))  # the x locations for the groups
+    width = 0.1  # the width of the bars
+    fig2, ax2 = plt.subplots()
+
+    i = 0
+    for statement in LIST_OF_STATEMENTS:
+        tmp = []
+        labels = []
+        stds = []
+        for m in means_compl:
+            if m.statem == statement:
+                tmp.append(m.mean)
+                labels.append(m.id)
+                stds.append(m.std)
+        ind2 = np.arange(len(tmp))
+        ax2.bar(ind2 - 0.3 + ((width) * i), tmp, width, color=LIST_OF_BAR_COLOURS[i % 10], edgecolor='black', label=statement)
+        i += 1
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax2.set_ylim(1, 5)   #y axis limits
+    ax2.set_ylabel('Scores (Complementary)')
+    ax2.set_xlabel('Video ID')
+    #ax2.set_title('Complementary Scores per Statement by Video ID')
+    ax2.set_xticks(ind2)
+    ax2.set_xticklabels(labels)
+    ax2.legend()
+    ax2.grid(which = 'major', axis='y', linestyle='--')
+    plt.show()
+
+
+def plot3():
+    #
+    # plot
+    # scatter
+    # - complementary values; without error bars
+    ind3 = np.arange(len(means_compl))  # the x locations for the groups
+    width = 0.1  # the width of the bars
+    fig3, ax3 = plt.subplots()
+
+    i = 0
+    for statement in LIST_OF_STATEMENTS:
+        tmp = []
+        labels = []
+        stds = []
+        for m in means_compl:
+            if m.statem == statement:
+                tmp.append(m.mean)
+                labels.append(m.id)
+                stds.append(m.std)
+        jj = np.arange(len(tmp))
+        ax3.scatter(jj , tmp, s = 50, marker = LIST_OF_MARKER_STYLES[i % 10], color=LIST_OF_BAR_COLOURS[i % 10], label = statement, alpha=1, edgecolors='none')
+        i += 1
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    #ax3.tick_params=(axis='x', which ='both', length=6, width=2)
+    ax3.set_ylim(1, 5)   #y axis limits
+    ax3.set_ylabel('Scores (Complementary)')
+    ax3.set_xlabel('Video ID')
+    #ax2.set_title('Complementary Scores per Statement by Video ID')
+    ax3.set_xticks(jj)
+    ax3.set_xticklabels(labels)
+    ax3.legend()
+    ax3.grid(which = 'major', axis='y', linestyle='--')
+    plt.show()
+
+
+
+
+
+
+
 
 
 # globals
@@ -115,70 +238,44 @@ for vid in sets.videos:
             vids_compl[s] = vid.__dict__[s]
         means_compl.append(Mean(vid.id, s, np.mean(vids_compl[s]), np.std(vids_compl[s])))
 
+
+#sort by video ID
+#means_compl.sort(key=lambda x: x.id, reverse=False)
+#means.sort(key=lambda x: x.id, reverse=False)
+#sort by order
+means_compl.sort(key=lambda x: x.order, reverse=False)
+means.sort(key=lambda x: x.order, reverse=False)
+
+
+
 #
 # plot
 # barchart
 #  - original values ; with error bars
-ind1 = np.arange(len(means))  # the x locations for the groups
-width = 0.1  # the width of the bars
-fig1, ax1 = plt.subplots()
+if SHOW_PLOT1:
+    plot1()
 
-i = 0
-for statement in LIST_OF_STATEMENTS:
-    tmp = []
-    labels = []
-    stds = []
-    for m in means:
-        if m.statem == statement:
-            tmp.append(m.mean)
-            labels.append(m.id)
-            stds.append(m.std)
-    ind1 = np.arange(len(tmp))
-    ax1.bar(ind1 - 0.3 + ((width) * i), tmp, width, yerr=stds, color=LIST_OF_BAR_COLOURS[i % 10], label=statement)
-    i += 1
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax1.set_ylim(1, 5)   #y axis limits
-ax1.set_ylabel('Scores')
-ax1.set_xlabel('Video ID')
-ax1.set_title('Scores per Statement by Video ID')
-ax1.set_xticks(ind1)
-ax1.set_xticklabels(labels)
-ax1.legend()
-ax1.grid(which = 'major', axis='y', linestyle='--')
-plt.show()
 
 
 #
 # plot
 # barchart
 # - complementary values; without error bars
-ind2 = np.arange(len(means_compl))  # the x locations for the groups
-width = 0.1  # the width of the bars
-fig2, ax2 = plt.subplots()
+if SHOW_PLOT2:
+    plot2()
 
-i = 0
-for statement in LIST_OF_STATEMENTS:
-    tmp = []
-    labels = []
-    stds = []
-    for m in means_compl:
-        if m.statem == statement:
-            tmp.append(m.mean)
-            labels.append(m.id)
-            stds.append(m.std)
-    ind2 = np.arange(len(tmp))
-    ax2.bar(ind1 - 0.3 + ((width) * i), tmp, width, color=LIST_OF_BAR_COLOURS[i % 10], label=statement)
-    i += 1
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax2.set_ylim(1, 5)   #y axis limits
-ax2.set_ylabel('Scores (Complementary)')
-ax2.set_xlabel('Video ID')
-ax2.set_title('Complementary Scores per Statement by Video ID')
-ax2.set_xticks(ind2)
-ax2.set_xticklabels(labels)
-ax2.legend()
-ax2.grid(which = 'major', axis='y', linestyle='--')
-plt.show()
+
+
+
+
+#
+# plot
+# scatter
+# - complementary values; without error bars
+if SHOW_PLOT3:
+    plot3()
+
+
 
 
 
@@ -186,3 +283,15 @@ plt.show()
 
 
 print('done')
+
+
+
+#other test stuff
+if FLUSH_MEANS_TO_FILE:
+    plimmplom = '['
+    for juju in means:
+        plimmplom += json.dumps(juju.__dict__)+',\n'
+    plimmplom += ']'
+    text_file.open("data_out.json", "w")
+    text_file.write(plimmplom)
+    text_file.close()
