aace6e899387dbadf55fa3819d7c27e1ce0abb48
emmanouil <emmanouil@users.noreply.github.com>
2019-02-04 16:56:56 +0100

    Update question_analysis_script.py

--- single-file_snippets/question_analysis_script.py
+++ single-file_snippets/question_analysis_script.py
@@ -3,8 +3,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-import pandas as pd
-import math
+from math import pi
 
 
 
@@ -23,7 +22,7 @@ LIST_OF_MARKER_STYLES = ['o', '^', 'v', 's', 'D', '*']
 SHOW_PLOT1 = False
 SHOW_PLOT2 = True
 SHOW_PLOT3 = False
-SHOW_PLOT4 = False
+SHOW_PLOT4 = True
 
 FLUSH_MEANS_TO_FILE = False
 
@@ -210,14 +209,14 @@ def plot4(video_ids):
     N = len(LIST_OF_STATEMENTS)
     
     # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
-    angles = [n / float(N) * 2 * math.pi for n in range(N)]
+    angles = [n / float(N) * 2 * pi for n in range(N)]
     angles += angles[:1]
     
     # Initialise the spider plot
     ax = plt.subplot(111, polar=True)
     
     # If you want the first axis to be on top:
-    ax.set_theta_offset(math.pi / 2)
+    ax.set_theta_offset(pi / 2)
     ax.set_theta_direction(-1)
     
     # Draw one axe per variable + add labels labels yet
